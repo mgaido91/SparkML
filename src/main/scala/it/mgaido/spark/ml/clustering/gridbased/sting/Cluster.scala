82880b8edc06fe9c4314814882ad2f4e38893095
speedymrk9 <marcogaido91@gmail.com>
2016-05-21 00:15:18 +0200

    Adding the cluster class

---
+++ src/main/scala/it/mgaido/spark/ml/clustering/gridbased/sting/Cluster.scala
@@ -0,0 +1,10 @@
+package it.mgaido.spark.ml.clustering.gridbased.sting
+
+import it.mgaido.spark.ml.clustering.ClusterId
+
+/**
+ * @author mark9
+ */
+class Cluster(val id:ClusterId, val cell:Cell) {
+  
+}
